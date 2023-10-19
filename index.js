d11b362d4ccfbc4815b46d13121cab5db00b8bc9
ohidursohag <ohidursohag9595@gmail.com>
2023-10-19 14:55:58 +0600

    Server SuccessFully Connected to Mongodb

--- index.js
+++ index.js
@@ -9,6 +9,39 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.bo45c0q.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+   serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+   }
+});
+
+async function run() {
+   try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+   } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+   }
+}
+run().catch(console.dir);
+
+
+
+
+
 app.get('/', (req, res) => { 
    res.send('Server returned  successfully')
 })
